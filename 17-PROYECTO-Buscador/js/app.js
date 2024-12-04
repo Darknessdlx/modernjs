0dbc255006afbfb4b18dba599ba245e9e495e63a
darknessdlx <darknessdlx@gmail.com>
2024-12-04 06:14:58 +0000

    First commit

---
+++ 17-PROYECTO-Buscador/js/app.js
@@ -0,0 +1,39 @@
+// Variables 
+const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+
+const max = new Date().getFullYear();
+const min = max - 10;
+
+
+// Eventos 
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarAutos(); // muestra los autos al cargar 
+
+    // llena las opciones de anios \
+    llenarSelect();
+
+})
+
+
+// Funciones
+function mostrarAutos () {
+    autos.forEach(auto => {
+
+        const { marca, modelo, year, puertas, transmision, precio, color } = auto;
+        const autoHTML = document.createElement('p');
+        autoHTML.textContent = `
+            ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmision: ${transmision} - Precio: ${precio} - Color: ${color}
+        `;
+
+        //insertar en el html
+        resultado.appendChild(autoHTML);
+    });
+}
+
+function llenarSelect() {
+    
+    for(let i = max; i > min; i++) {
+        console.log(i)
+    }
+}; 
