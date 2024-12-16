a1d0f99bb5d79f9732fe1d78fe9ead3dffe3a6fa
darknessdlx <darknessdlx@gmail.com>
2024-12-16 08:19:26 -0600

    finalizando proyecto

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -21,13 +21,13 @@ const datosBusqueda = {
     maximo: '',
     puertas: '',
     transmision: '',
-    color: ''
+    color: '',
 }
 
 
 // Eventos 
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarAutos(); // muestra los autos al cargar 
+    mostrarAutos(autos); // muestra los autos al cargar
 
     // llena las opciones de anios
     llenarSelect();
@@ -37,10 +37,51 @@ document.addEventListener('DOMContentLoaded', () => {
 // Event listener para los select de busqueda
 marca.addEventListener('change', e => {
     datosBusqueda.marca = e.target.value;
+
+    filtrarAuto();
+})
+
+year.addEventListener('change', e => {
+    datosBusqueda.year = parseInt(e.target.value);
+
+    filtrarAuto();
+})
+
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;
+
+    filtrarAuto();
+})
+
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;
+
+    filtrarAuto();
+})
+
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = parseInt(e.target.value);
+
+    filtrarAuto();
+})
+
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value;
+
+    filtrarAuto();
+})
+
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
+
+    filtrarAuto();
 })
 
 // Funciones
-function mostrarAutos () {
+function mostrarAutos (autos) {
+
+    limpiarHTML();
+
     autos.forEach(auto => {
 
         const { marca, modelo, year, puertas, transmision, precio, color } = auto;
@@ -54,15 +95,92 @@ function mostrarAutos () {
     });
 }
 
+function limpiarHTML() {
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
 function llenarSelect() {
     
-    for(let i = max; i >= min; i--) {
+    for( let i = max; i >= min; i-- ) {
         const opcion = document.createElement('option');
         opcion.value = i;
         opcion.textContent = i;
-        year.appendChild(opcion) // Agrega las opciones de anio al select
+        year.appendChild(opcion); // Agrega las opciones de anio al select
 
     }
 }
 
-// cuarta prueba
+// Function que filtra con base en la búsqueda
+function filtrarAuto() {
+    const resultado = autos.filter( filtrarMarca ).filter( filterYear ).filter( filterMinPrice ).filter( filterMaxPrice ).filter( filterDoors ).filter( filterTransmision ).filter( filterColor );
+
+    // console.log(resultado);
+
+    if ( resultado.length ) {
+        mostrarAutos(resultado);
+    } else {
+        noResultado();
+    }
+
+}
+
+function noResultado() {
+    limpiarHTML();
+
+    const noResultado = document.createElement('div');
+    noResultado.classList.add('alerta', 'error');
+    noResultado.textContent = 'No hay resultados';
+    resultado.appendChild(noResultado);
+}
+
+function filtrarMarca(auto) {
+    const { marca } = datosBusqueda;
+    if ( marca ) {
+        return auto.marca === marca;
+    }
+    return auto;
+}
+
+function filterYear(auto) {
+    const { year } = datosBusqueda;
+    if ( year ) {
+        return auto.year === year;
+    } return auto;
+}
+
+function filterMinPrice(auto) {
+    const { minimo } = datosBusqueda;
+    if ( minimo ) {
+        return auto.precio >= minimo;
+    } return auto;
+}
+
+function filterMaxPrice(auto) {
+    const { maximo } = datosBusqueda;
+    if ( maximo ) {
+        return auto.precio <= maximo;
+    } return auto;
+}
+
+function filterDoors(auto) {
+    const { puertas } = datosBusqueda;
+    if ( puertas ) {
+        return auto.puertas === puertas;
+    } return auto;
+}
+
+function filterTransmision(auto) {
+    const { transmision } = datosBusqueda;
+    if ( transmision ) {
+        return auto.transmision === transmision;
+    } return auto;
+}
+
+function filterColor(auto) {
+    const { color } = datosBusqueda;
+    if ( color ) {
+        return auto.color === color;
+    } return auto;
+}
